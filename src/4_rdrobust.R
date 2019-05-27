1a9ca0d4fce2fdc5bf58af699aa08161c196cdef
Paula Pereda <ppereda@transformauruguay.gub.uy>
2019-05-27 14:30:34 -0300

    outcome it's fine!

--- src/4_rdrobust.R
+++ src/4_rdrobust.R
@@ -13,16 +13,15 @@ X <- data$share_fem
 T <- data$treatment
 T_X <- T*X
 
-out <- rdplot(Y, X, nbins = c(10,10), c = 0.5, 
-              binselect = 'esmv',
-              cex.axis = 1.5, cex.lab = 1.5)
+out <- rdplot(Y, X, nbins = c(10,10), 
+              c = 0.5, binselect = 'esmv')
 summary(out)
 
 ## 20 Quantile-spaced bins
 
-out <- rdplot(Y, X, nbins = c(10,10), c = 0.5,
+out <- rdplot(Y, X, nbins = c(10,10), c = 0.4,
              binselect = 'qs', cex.axis = 1.5, 
-             cex.lab = 1.5, x.label = 'Share earned by the wife',
+             cex.lab =1.5, x.label = 'Share earned by the wife',
              y.label = 'Fraction of couples', 
              title = 'Distribution of Relative Income (ECH data)') 
 
