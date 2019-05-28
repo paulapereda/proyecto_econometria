9372f968a9b82f8b2c9524f3bd5cc0cbea3d19fe
Paula Pereda <ppereda@transformauruguay.gub.uy>
2019-05-28 09:42:45 -0300

    adds latex dir

--- src/4_rdrobust.R
+++ src/4_rdrobust.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(rdrobust)
+library(rdd)
 
 source(here::here("src", "3_tidy.R"))
 
@@ -14,15 +15,25 @@ out_yt <- rdplot(Y, X, nbins = c(10,10),
               c = 0.5, binselect = 'esmv')
 summary(out_yt)
 
-## 20 Quantile-spaced bins (bins del paper)
+## (i) 20 Quantile-spaced bins (n° bins del paper)
 
 out_yt <- rdplot(Y, X, nbins = c(10,10), c = 0.500000001,
              binselect = 'qs', cex.axis = 1.5, 
              cex.lab = 1.5, x.label = 'Share earned by the wife',
              y.label = 'Number of couples', 
-             title = 'Distribution of Relative Income (ECH data)') 
+             title = 'Distribution of Relative Income (ECH-EUT data, 2013)') 
 
 summary(out_yt)
+
+## (ii) Probando con el paquete RDD
+
+reg_yt <- RDestimate(outcome ~ share_fem, data = data_yt, cutpoint = 0.500000001)
+
+plot(reg_yt)
+
+title(main = "Distribution of Relative Income (ECH-EUT data, 2013)", 
+      xlab = "Share earned by the wife",
+      ylab = "Number of couples")
 
 # Segundo intento: ingresos laborales (yl)
 
@@ -35,7 +46,7 @@ out_yl <- rdplot(Y, X, nbins = c(10,10),
               c = 0.5, binselect = 'esmv')
 summary(out_yl)
 
-## 20 Quantile-spaced bins (bins del paper)
+## (i) 20 Quantile-spaced bins (n° bins del paper)
 
 out_yl <- rdplot(Y, X, nbins = c(10,10), c = 0.500000001,
               binselect = 'qs', cex.axis = 1.5, 
@@ -44,3 +55,14 @@ out_yl <- rdplot(Y, X, nbins = c(10,10), c = 0.500000001,
               title = 'Distribution of Relative Income (ECH data)') 
 
 summary(out_yl)
+
+## (ii) Probando con el paquete RDD
+
+reg_yl <- RDestimate(outcome2 ~ share_fem2, data = data_yl, cutpoint = 0.500000001)
+
+plot(reg_yl)
+
+title(main = "Distribution of Relative Income (ECH-EUT data, 2013)", 
+      xlab = "Share earned by the wife",
+      ylab = "Number of couples")
+
